3d4e8f1dfbfbe32b98989555b2e59dfc592af145
Madhucrj3 <madhureshcrj3@gmail.com>
2023-03-16 17:07:58 +0530

    app delete commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,29 @@
-import React from 'react';
-import './App.css';
-import styled from 'styled-components';
-import Input from './components/Input';
-import { useEffect, useState } from 'react';
-import MyTask from './components/MyTask';
-import { inject, observer } from 'mobx-react';
+import React from "react";
+import styled from "styled-components";
+import Input from "./components/Input";
+import { useEffect, useState } from "react";
+import MyTask from "./components/MyTask";
+import { inject, observer } from "mobx-react";
 const Heading = styled.h1`
-color: #000;
-text-align: center;
-font-family: "Roboto";
-font-size: 46px;
-font-weight: 500;
-margin-top: 20px;
-margin-bottom: 20px
+  color: #000;
+  text-align: center;
+  font-family: "Roboto";
+  font-size: 46px;
+  font-weight: 500;
+  margin-top: 20px;
+  margin-bottom: 20px;
 `;
 
-const App= inject("store")(
+const App = inject("store")(
   observer((props) => {
-  return (
-    <div className="App">
-       <Heading>ToDos</Heading>
-       <Input />
-       <MyTask  />
-    </div>
-  );
-})
+    return (
+      <div className="App">
+        <Heading>ToDos</Heading>
+        <Input />
+        <MyTask />
+      </div>
+    );
+  })
 );
 
 export default App;
